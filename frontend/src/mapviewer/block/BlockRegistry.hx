db1d9353ace8612beb109f48ba45c796e06d1737
Thinkofdeath <thethinkofdeath@gmail.com>
2014-03-11 22:56:00 +0000

    Fix indentation

--- frontend/src/mapviewer/block/BlockRegistry.hx
+++ frontend/src/mapviewer/block/BlockRegistry.hx
@@ -511,19 +511,19 @@ class BlockRegistry {
 		for (flag in [0, 4]) {
 			for (rot in 0 ... 4) {
 				registerBlock('bed_bottom_${rot}_${flag}', new Block().chainBlock()
-						.model(BlockBed.modelBottom.clone().rotateY(90 * rot))
-						.solid(false)
-						.ret())
-						.legacyId(26)
-						.dataValue(rot | flag)
-						.build();
+					.model(BlockBed.modelBottom.clone().rotateY(90 * rot))
+					.solid(false)
+					.ret())
+					.legacyId(26)
+					.dataValue(rot | flag)
+					.build();
 				registerBlock('bed_top_${rot}_${flag}', new Block().chainBlock()
-						.model(BlockBed.modelTop.clone().rotateY(90 * rot))
-						.solid(false)
-						.ret())
-						.legacyId(26)
-						.dataValue(rot | flag | 8)
-						.build();
+					.model(BlockBed.modelTop.clone().rotateY(90 * rot))
+					.solid(false)
+					.ret())
+					.legacyId(26)
+					.dataValue(rot | flag | 8)
+					.build();
 			}
 		}
 		
