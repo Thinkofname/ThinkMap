6547fecc7610ae4863da7007a39e258a0b6fc8a3
Thinkofdeath <thethinkofdeath@gmail.com>
2014-03-23 13:40:42 +0000

    Simplify the logger

--- frontend/src/mapviewer/logging/Logger.hx
+++ frontend/src/mapviewer/logging/Logger.hx
@@ -31,29 +31,24 @@ class Logger {
     @:extern
     inline public function info(txt : String) {
         if (DEBUG_MODE && CAN_LOG)
-            log(txt, "color:#00FFFF; background: black;");
+            trace('[INFO][$name]: $txt');
     }
 
     @:extern
     inline public function debug(txt : String) {
         if (DEBUG_MODE && CAN_LOG)
-            log(txt, "color:yellow; background: black;");
+            trace('[DEBUG][$name]: $txt');
     }
 
     @:extern
     inline public function warn(txt : String) {
         if (CAN_LOG)
-            log(txt, "color:#F27900; background: black; font-weight: bold;");
+            trace('[WARN][$name]: $txt');
     }
 
     @:extern
     inline public function error(txt : String) {
         if (CAN_LOG)
-            log(txt, "color:black; background: red; font-weight: bold;");
-    }
-
-    @:extern
-    inline private function log(msg : String, format : String) {
-        untyped console.log('%c[$name]: $msg', format);
+            trace('[ERROR][$name]: $txt');
     }
 }
