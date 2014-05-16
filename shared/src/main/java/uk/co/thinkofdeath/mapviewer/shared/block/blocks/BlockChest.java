ccc2928580fd2cda1fa02ff318e303e5d71924f4
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-05-17 00:04:53 +0100

    Make chests use the new texture style

--- shared/src/main/java/uk/co/thinkofdeath/mapviewer/shared/block/blocks/BlockChest.java
+++ shared/src/main/java/uk/co/thinkofdeath/mapviewer/shared/block/blocks/BlockChest.java
@@ -17,6 +17,7 @@
 package uk.co.thinkofdeath.mapviewer.shared.block.blocks;
 
 import uk.co.thinkofdeath.mapviewer.shared.Face;
+import uk.co.thinkofdeath.mapviewer.shared.ForEachIterator;
 import uk.co.thinkofdeath.mapviewer.shared.IMapViewer;
 import uk.co.thinkofdeath.mapviewer.shared.Texture;
 import uk.co.thinkofdeath.mapviewer.shared.block.Block;
@@ -25,28 +26,19 @@ import uk.co.thinkofdeath.mapviewer.shared.block.StateMap;
 import uk.co.thinkofdeath.mapviewer.shared.block.states.EnumState;
 import uk.co.thinkofdeath.mapviewer.shared.model.Model;
 import uk.co.thinkofdeath.mapviewer.shared.model.ModelFace;
+import uk.co.thinkofdeath.mapviewer.shared.model.ModelVertex;
 
 public class BlockChest extends BlockFactory {
 
     public static final String FACING = "facing";
 
-    private final Texture chestSideFront;
-    private final Texture chestSide;
-    private final Texture chestBottomTop;
-    private final Texture chestTop;
-    private final Texture chestTopBottom;
-    private final Texture chestLock;
+    private final Texture chest;
 
     public BlockChest(IMapViewer iMapViewer) {
         super(iMapViewer);
         addState(FACING, new EnumState(Facing.class));
 
-        chestSideFront = iMapViewer.getTexture("chest_side_front");
-        chestSide = iMapViewer.getTexture("chest_side");
-        chestBottomTop = iMapViewer.getTexture("chest_bottom_top");
-        chestTop = iMapViewer.getTexture("chest_top");
-        chestTopBottom = iMapViewer.getTexture("chest_top_bottom");
-        chestLock = iMapViewer.getTexture("chest_lock");
+        chest = iMapViewer.getTexture("chest_normal");
     }
 
     public static enum Facing {
@@ -85,46 +77,74 @@ public class BlockChest extends BlockFactory {
                 model = new Model();
 
                 // Bottom
-                model.addFace(new ModelFace(Face.FRONT, chestSideFront, 1, 0, 14, 10, 15)
-                        .setTextureSize(0, 0, 14, 10));
-                model.addFace(new ModelFace(Face.BACK, chestSide, 1, 0, 14, 10, 1)
-                        .setTextureSize(0, 0, 14, 10));
-                model.addFace(new ModelFace(Face.LEFT, chestSide, 1, 0, 14, 10, 15)
-                        .setTextureSize(0, 0, 14, 10));
-                model.addFace(new ModelFace(Face.RIGHT, chestSide, 1, 0, 14, 10, 1)
-                        .setTextureSize(0, 0, 14, 10));
-                model.addFace(new ModelFace(Face.TOP, chestBottomTop, 1, 1, 14, 14, 10)
-                        .setTextureSize(0, 0, 14, 14));
-                model.addFace(new ModelFace(Face.BOTTOM, chestTop, 1, 1, 14, 14, 0, true)
-                        .setTextureSize(0, 0, 14, 14));
+                model.addFace(new ModelFace(Face.FRONT, chest, 1, 0, 14, 10, 15)
+                        .setTextureSize(0, 0, 14, 10)
+                        .forEach(new ForEachIterator<ModelVertex>() {
+                            @Override
+                            public void run(ModelVertex v) {
+                                v.setTextureX(v.getTextureX() / 4f + 14f / 64f);
+                                v.setTextureY(v.getTextureY() / 4f + 33f / 64f);
+                            }
+                        }));
+                model.addFace(new ModelFace(Face.BACK, chest, 1, 0, 14, 10, 1)
+                        .setTextureSize(0, 0, 14, 10).forEach(sideTexture));
+                model.addFace(new ModelFace(Face.LEFT, chest, 1, 0, 14, 10, 15)
+                        .setTextureSize(0, 0, 14, 10).forEach(sideTexture));
+                model.addFace(new ModelFace(Face.RIGHT, chest, 1, 0, 14, 10, 1)
+                        .setTextureSize(0, 0, 14, 10).forEach(sideTexture));
+                model.addFace(new ModelFace(Face.TOP, chest, 1, 1, 14, 14, 10)
+                        .setTextureSize(0, 0, 14, 14)
+                        .forEach(new ForEachIterator<ModelVertex>() {
+                            @Override
+                            public void run(ModelVertex v) {
+                                v.setTextureX(v.getTextureX() / 4f + 14f / 64f);
+                                v.setTextureY(v.getTextureY() / 4f + 19f / 64f);
+                            }
+                        }));
+                model.addFace(new ModelFace(Face.BOTTOM, chest, 1, 1, 14, 14, 0, true)
+                        .setTextureSize(0, 0, 14, 14).forEach(bottomTexture));
 
                 // Top
                 Model modelTop = new Model();
-                modelTop.addFace(new ModelFace(Face.FRONT, chestSideFront, 1, 0, 14, 5, 15)
-                        .setTextureSize(0, 10, 14, 5));
-                modelTop.addFace(new ModelFace(Face.BACK, chestSide, 1, 0, 14, 5, 1)
-                        .setTextureSize(0, 10, 14, 5));
-                modelTop.addFace(new ModelFace(Face.LEFT, chestSide, 1, 0, 14, 5, 15)
-                        .setTextureSize(0, 10, 14, 5));
-                modelTop.addFace(new ModelFace(Face.RIGHT, chestSide, 1, 0, 14, 5, 1)
-                        .setTextureSize(0, 10, 14, 5));
-                modelTop.addFace(new ModelFace(Face.TOP, chestTop, 1, 1, 14, 14, 5)
-                        .setTextureSize(0, 0, 14, 14));
-                modelTop.addFace(new ModelFace(Face.BOTTOM, chestTopBottom, 1, 1, 14, 14, 0, true)
-                        .setTextureSize(0, 0, 14, 14));
+                modelTop.addFace(new ModelFace(Face.FRONT, chest, 1, 0, 14, 5, 15)
+                        .setTextureSize(0, 0, 14, 5)
+                        .forEach(new ForEachIterator<ModelVertex>() {
+                            @Override
+                            public void run(ModelVertex v) {
+                                v.setTextureX(v.getTextureX() / 4f + 14f / 64f);
+                                v.setTextureY(v.getTextureY() / 4f + 14f / 64f);
+                            }
+                        }));
+                modelTop.addFace(new ModelFace(Face.BACK, chest, 1, 0, 14, 5, 1)
+                        .setTextureSize(0, 0, 14, 5).forEach(sideSmallTexture));
+                modelTop.addFace(new ModelFace(Face.LEFT, chest, 1, 0, 14, 5, 15)
+                        .setTextureSize(0, 0, 14, 5).forEach(sideSmallTexture));
+                modelTop.addFace(new ModelFace(Face.RIGHT, chest, 1, 0, 14, 5, 1)
+                        .setTextureSize(0, 0, 14, 5).forEach(sideSmallTexture));
+                modelTop.addFace(new ModelFace(Face.TOP, chest, 1, 1, 14, 14, 5)
+                        .setTextureSize(0, 0, 14, 14).forEach(bottomTexture));
+                modelTop.addFace(new ModelFace(Face.BOTTOM, chest, 1, 1, 14, 14, 0, true)
+                        .setTextureSize(0, 0, 14, 14)
+                        .forEach(new ForEachIterator<ModelVertex>() {
+                            @Override
+                            public void run(ModelVertex v) {
+                                v.setTextureX(v.getTextureX() / 4f + 28f / 64f);
+                                v.setTextureY(v.getTextureY() / 4f);
+                            }
+                        }));
 
 
                 // Lock
-                modelTop.addFace(new ModelFace(Face.FRONT, chestLock, 7, -2, 2, 4, 16, true)
-                        .setTextureSize(1, 1, 2, 4));
-                modelTop.addFace(new ModelFace(Face.LEFT, chestLock, 15, -2, 1, 4, 9)
-                        .setTextureSize(0, 1, 1, 4));
-                modelTop.addFace(new ModelFace(Face.RIGHT, chestLock, 15, -2, 1, 4, 7)
-                        .setTextureSize(0, 1, 1, 4));
-                modelTop.addFace(new ModelFace(Face.TOP, chestLock, 7, 15, 2, 1, 2)
-                        .setTextureSize(1, 0, 2, 1));
-                modelTop.addFace(new ModelFace(Face.BOTTOM, chestLock, 7, 15, 2, 1, -2)
-                        .setTextureSize(3, 0, 2, 1));
+                modelTop.addFace(new ModelFace(Face.FRONT, chest, 7, -2, 2, 4, 16, true)
+                        .setTextureSize(1, 1, 2, 4).forEach(lockTexture));
+                modelTop.addFace(new ModelFace(Face.LEFT, chest, 15, -2, 1, 4, 9)
+                        .setTextureSize(0, 1, 1, 4).forEach(lockTexture));
+                modelTop.addFace(new ModelFace(Face.RIGHT, chest, 15, -2, 1, 4, 7)
+                        .setTextureSize(0, 1, 1, 4).forEach(lockTexture));
+                modelTop.addFace(new ModelFace(Face.TOP, chest, 7, 15, 2, 1, 2)
+                        .setTextureSize(1, 0, 2, 1).forEach(lockTexture));
+                modelTop.addFace(new ModelFace(Face.BOTTOM, chest, 7, 15, 2, 1, -2)
+                        .setTextureSize(3, 0, 2, 1).forEach(lockTexture));
 
                 model.join(modelTop, 0, 9, 0);
                 model.rotateY(90 * facing.rotation);
@@ -137,4 +157,33 @@ public class BlockChest extends BlockFactory {
             return this.<Facing>getState(FACING).ordinal() + 2;
         }
     }
+
+    private static final ForEachIterator<ModelVertex> lockTexture = new ForEachIterator<ModelVertex>() {
+        @Override
+        public void run(ModelVertex v) {
+            v.setTextureX(v.getTextureX() / 4f);
+            v.setTextureY(v.getTextureY() / 4f);
+        }
+    };
+    private static final ForEachIterator<ModelVertex> sideTexture = new ForEachIterator<ModelVertex>() {
+        @Override
+        public void run(ModelVertex v) {
+            v.setTextureX(v.getTextureX() / 4f);
+            v.setTextureY(v.getTextureY() / 4f + 33f / 64f);
+        }
+    };
+    private static final ForEachIterator<ModelVertex> bottomTexture = new ForEachIterator<ModelVertex>() {
+        @Override
+        public void run(ModelVertex v) {
+            v.setTextureX(v.getTextureX() / 4f + 14f / 64f);
+            v.setTextureY(v.getTextureY() / 4f);
+        }
+    };
+    private static final ForEachIterator<ModelVertex> sideSmallTexture = new ForEachIterator<ModelVertex>() {
+        @Override
+        public void run(ModelVertex v) {
+            v.setTextureX(v.getTextureX() / 4f);
+            v.setTextureY(v.getTextureY() / 4f + 14f / 64f);
+        }
+    };
 }
