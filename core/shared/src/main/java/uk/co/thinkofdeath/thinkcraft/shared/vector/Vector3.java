790a4244c8ba8314f22e9e076cbe12a51c43fbf2
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-07-21 14:10:31 +0100

    Modify Vector3 to use FloatBuffers

--- core/shared/src/main/java/uk/co/thinkofdeath/thinkcraft/shared/vector/Vector3.java
+++ core/shared/src/main/java/uk/co/thinkofdeath/thinkcraft/shared/vector/Vector3.java
@@ -16,41 +16,44 @@
 
 package uk.co.thinkofdeath.thinkcraft.shared.vector;
 
+import uk.co.thinkofdeath.thinkcraft.shared.platform.Platform;
+import uk.co.thinkofdeath.thinkcraft.shared.platform.buffers.FloatBuffer;
+
 public class Vector3 {
 
-    float[] values = new float[3];
+    private FloatBuffer values = Platform.createFloatBuffer(3);
 
     public Vector3() {
 
     }
 
     public void apply(Matrix4 matrix) {
-        float x = values[0];
-        float y = values[1];
-        float z = values[2];
+        float x = values.get(0);
+        float y = values.get(1);
+        float z = values.get(2);
         float w = matrix.get(3) * x + matrix.get(7) * y + matrix.get(11) * z + matrix.get(15);
         w = w == 0 ? 1 : w;
 
-        values[0] = (matrix.get(0) * x + matrix.get(4) * y + matrix.get(8) * z + matrix.get(12)) / w;
-        values[1] = (matrix.get(1) * x + matrix.get(5) * y + matrix.get(9) * z + matrix.get(13)) / w;
-        values[2] = (matrix.get(2) * x + matrix.get(6) * y + matrix.get(10) * z + matrix.get(14)) / w;
+        values.set(0, (matrix.get(0) * x + matrix.get(4) * y + matrix.get(8) * z + matrix.get(12)) / w);
+        values.set(1, (matrix.get(1) * x + matrix.get(5) * y + matrix.get(9) * z + matrix.get(13)) / w);
+        values.set(2, (matrix.get(2) * x + matrix.get(6) * y + matrix.get(10) * z + matrix.get(14)) / w);
     }
 
     public void set(float x, float y, float z) {
-        values[0] = x;
-        values[1] = y;
-        values[2] = z;
+        values.set(0, x);
+        values.set(1, y);
+        values.set(2, z);
     }
 
     public float getX() {
-        return values[0];
+        return values.get(0);
     }
 
     public float getY() {
-        return values[1];
+        return values.get(1);
     }
 
     public float getZ() {
-        return values[2];
+        return values.get(2);
     }
 }
