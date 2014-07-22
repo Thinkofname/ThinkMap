0d77b789faad0a7933f0e74dffcdb8f928c038cb
Thinkofdeath <thinkofdeath@spigotmc.org>
2014-07-22 23:56:49 +0100

    Add a timeout to the connection

--- bukkit/src/main/java/uk/co/thinkofdeath/thinkcraft/bukkit/web/ServerChannelInitializer.java
+++ bukkit/src/main/java/uk/co/thinkofdeath/thinkcraft/bukkit/web/ServerChannelInitializer.java
@@ -23,6 +23,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import uk.co.thinkofdeath.thinkcraft.bukkit.ThinkMapPlugin;
 
 public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
@@ -36,6 +37,7 @@ public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> 
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("timeout", new ReadTimeoutHandler(15));
         pipeline.addLast("codec-http", new HttpServerCodec());
         pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
         pipeline.addLast("handler", new HTTPHandler(plugin));
